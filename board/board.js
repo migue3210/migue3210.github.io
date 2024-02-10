332440f6e4a1d5af5050376bfc9193586daf5a25
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-02-09 21:00:41 -0400

    resizes card according to the matrix

--- board/board.js
+++ board/board.js
@@ -1,7 +1,11 @@
+function generateSize() {
+    return Math.floor(Math.random() * 3) + 3;
+}
+const size = generateSize();
 function randomNumber() {
     const numList = [];
 
-    while (numList.length < 25) {
+    while (numList.length < size * size) {
         const randomNumber = Math.floor(Math.random() * 50) + 1;
         if (!numList.includes(randomNumber)) {
             numList.push(randomNumber);
@@ -10,6 +14,16 @@ function randomNumber() {
 
     return numList;
 }
+
+if (size == 5) {
+    document.getElementById('number-list').className = 'number-list-5';
+} else if (size == 4) {
+    document.getElementById('number-list').className = 'number-list-4';
+} else if (size == 3) {
+    document.getElementById('number-list').className = 'number-list-3';
+}
+
+
 
 const verifyNumber = randomNumber();
 const numbers = randomNumber();
@@ -22,9 +36,6 @@ numbers.forEach((number) => {
     div.id = `${numbers.indexOf(number)}`;
     div.textContent = `${number}`;
     container.appendChild(div);
-    // if (number in verifyNumber) {
-    //     div.className = 'number-validate number';
-    // }
 });
 
 
@@ -39,7 +50,6 @@ function bingoCall() {
         }
     }
     counter++;
-    // console.log(counter)
 }
 
 
