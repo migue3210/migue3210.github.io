47a6c379f67fd0088be7aa1ac4e3faba15ff75b6
Miguel Jiménez <69486469+migue3210@users.noreply.github.com>
2024-02-09 20:13:49 -0400

    adds bingo call :8ball:

--- board/board.js
+++ board/board.js
@@ -19,13 +19,30 @@ const container = document.getElementById('number-list');
 numbers.forEach((number) => {
     const div = document.createElement('div');
     div.className = 'number';
+    div.id = `${numbers.indexOf(number)}`;
     div.textContent = `${number}`;
     container.appendChild(div);
-    if (number in verifyNumber) {
-        div.className = 'number-validate number';
+    // if (number in verifyNumber) {
+    //     div.className = 'number-validate number';
+    // }
+});
+
+
+let counter = 0
+number = document.getElementById("bingo-call-number");
+function bingoCall() {
+    if (counter < 25) {
+        number.innerText = verifyNumber[counter];
+        if (numbers.includes(verifyNumber[counter])) {
+            numberVerify = document.getElementById(`${numbers.indexOf(verifyNumber[counter])}`);
+            numberVerify.className = 'number-validate number';
+        }
     }
-});
+    counter++;
+    // console.log(counter)
+}
 
-numbers.forEach((number) => {
 
-});
+
+
+
